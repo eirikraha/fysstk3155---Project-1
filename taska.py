c9481fa662312d67fedb6aa5f3a081521fa12e2b
joakika <43650748+joakika@users.noreply.github.com>
2018-09-27 19:25:59 +0200

    hei

--- taska.py
+++ taska.py
@@ -13,7 +13,7 @@ def FrankeFunction(x,y):
     term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
     return term1 + term2 + term3 + term4
-
+# HEi
 def variance(y, y_hat, p):
 	# Based upon the calculation of sigma^2 on page
 	# 47 in HTF
