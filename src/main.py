13885f29a6e2f7d6d35d4b98878f3c3302782410
eirikraha <eirikhauge@hotmail.com>
2018-10-07 16:03:19 +0200

    Divided taska.py into three scripts

---
+++ src/main.py
@@ -0,0 +1,302 @@
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.linear_model import Lasso, Ridge, LinearRegression
+from sklearn.utils import resample
+from sklearn.model_selection import train_test_split
+from sys import stdout, argv, exit
+from imageio import imread
+from classes import *
+from functions import *
+
+################################
+#####     Initializing     #####
+################################
+
+if len(argv) == 1:
+	print ("Please write a, b, c, d or e after the filename in terminal")
+	print ("You can also write plot after the filename if you wish.")
+	print ("Plotting only applies for task b and c")
+	exit()
+elif len(argv) == 2:
+	if argv[1] == "b" or argv[1] == "c":
+		print ("If you want to plot, please write plot after task %s" %argv[1])
+
+	argv.append("noplot")
+
+fs1 = 20
+fs2 = 16
+
+
+n_samples = 100
+
+x_start = np.random.rand(n_samples, 1)
+y_start = np.random.rand(n_samples, 1)
+x_mesh, y_mesh = np.meshgrid(x_start,y_start)
+
+
+z_mesh = FrankeFunction(x_mesh, y_mesh)
+
+x = np.ravel(x_mesh)
+y = np.ravel(y_mesh)
+
+zr = z_mesh.reshape(-1, 1)
+zr_noise = zr + 0.01*np.random.randn(n_samples**2, 1)
+
+# Centering x and y. Why do we need to do this?
+# This is added to give a common frame of reference, not needed
+# in this project, but might be used later
+#x_ = x - np.mean(x)
+#y_ = y - np.mean(y)
+#z_ = z - np.mean(z)
+
+#Creating the vector
+#np.c_ sets each element as a column
+X = X_creator(x, y)
+
+
+###########################
+#####     Task a)     #####
+###########################
+
+if argv[1] == "a" or argv == "all":
+	#Without noise
+
+	OLS = HomeMadeOLS().fit(X, zr).predict(X)
+	
+	pred_LS = (OLS.pred).flatten()
+	#beta_conf = OLS.ConfIntBeta(X, zr, pred_LS).conf_intervals
+
+	pred_LS_SK = (LinearRegression(fit_intercept = False).fit(X, zr).predict(X)).flatten()
+
+	#With noise
+	pred_LS_noise = (HomeMadeOLS().fit(X, zr_noise).predict(X).pred).flatten()
+	pred_LS_noise_SK = (LinearRegression(fit_intercept = False).fit(X, zr_noise).predict(X)).flatten()
+
+	#Finding MSE and R2
+	R2MSEeval(zr, pred_LS, method = "OLS", printer = True, sk = True)   #Should we compare with zr_noise?"
+	R2MSEeval(zr, pred_LS_SK, method = "OLS from SK", printer = True, sk = True)
+	R2MSEeval(zr, pred_LS_noise, method = "OLS with noise", printer = True, sk = True)
+	R2MSEeval(zr, pred_LS_noise_SK, method = "OLS from SK with noise", printer = True, sk = True)
+
+
+	print ("Bootstrap using OLS without noise:")
+	bootstrap(X, zr, method = "OLS")
+	print ("Bootstrap using OLS with noise:")
+	bootstrap(X, zr_noise, method = "OLS")
+
+###########################
+#####     Task b)     #####
+###########################
+
+elif argv[1] == "b" or argv == "all":
+	if argv[2] == "plot":
+		#Finding optimal lambda value for the instance without noise
+		lmb_values = np.logspace(-5, -1, 100)   #Should we use logspace?
+		R2list = []
+		MSElist = []
+
+		print (len(lmb_values))
+
+		for lmb in lmb_values:
+			MSE, R2 = bootstrap(X, zr, lmb = lmb, method = "Ridge", sk = True, 
+								output = True, printer = False)
+			R2list.append(R2)
+			MSElist.append(MSE)
+
+
+		fig, ax = plt.subplots()
+
+		ax.semilogx(lmb_values, R2list, label = "R2")
+		ax.set_title(r"R2 as a function of $\lambda$", fontsize = fs1)
+		ax.set_xlabel(r"$\lambda$", fontsize = fs2)
+		ax.set_ylabel(r"R2", fontsize = fs2)
+		ax.legend()
+
+		fig.savefig('../figures/RidgeR2.png')
+
+		fig2, ax2 = plt.subplots()
+		ax2.semilogx(lmb_values, MSElist, label = "MSE") 
+		ax2.set_title(r"MSE as a function of $\lambda$", fontsize = fs1)
+		ax2.set_xlabel(r"$\lambda$", fontsize = fs2)
+		ax2.set_ylabel(r"MSE", fontsize = fs2)
+		ax2.legend()
+
+		fig2.savefig('../figures/RidgeMSE.png')
+		plt.show()
+
+		#Finds the best lambda to be about 1e-4
+
+
+	#Without noise
+	pred_ridge = (HomeMadeRidge().fit(X, zr, lmb = 1e-4).predict(X).pred).flatten()
+	pred_ridge_SK = (Ridge(fit_intercept = False).fit(X, zr).predict(X)).flatten()
+
+	#With noise
+
+	#OBS! Remeber to vary noise
+
+	pred_ridge_noise = (HomeMadeRidge().fit(X, zr_noise, lmb = 1e-4).predict(X).pred).flatten()
+	pred_ridge_noise_SK = (Ridge(fit_intercept = False).fit(X, zr_noise).predict(X)).flatten()
+
+	#Finding MSE and R2
+	R2MSEeval(zr, pred_ridge, method = "Ridge", printer = True, sk = True)
+	R2MSEeval(zr, pred_ridge_SK, method = "Ridge from SK", printer = True, sk = True)
+	R2MSEeval(zr, pred_ridge_noise, method = "Ridge with noise", printer = True, sk = True)
+	R2MSEeval(zr, pred_ridge_noise_SK, method = "Ridge from SK with noise", printer = True, sk = True)
+
+
+	print ("Bootstrap using Ridge without noise:")
+	bootstrap(X, zr, lmb = 1e-4, method = "Ridge")
+	print ("Bootstrap using Ridge with noise:")
+	bootstrap(X, zr_noise, lmb = 1e-4, method = "Ridge")
+
+
+###########################
+#####     Task c)     #####
+###########################
+
+elif argv[1] == "c" or argv == "all":
+
+	#Plots if neccessary
+	if len(argv) > 1 and argv[2] == "plot":
+		#Finding optimal alpha value for the instance without noise
+		alpha_values = np.logspace(-4, -1, 10)   #Should we use logspace?
+		R2list = []
+		MSElist = []
+
+		for alpha in alpha_values:
+			MSE, R2 = bootstrap(X, zr, alpha = alpha, method = "Lasso", sk = True, 
+								output = True, printer = False)
+			R2list.append(R2)
+			MSElist.append(MSE)
+
+
+		fig, ax = plt.subplots()
+
+		ax.semilogx(alpha_values, R2list, label = "R2")
+		ax.set_title(r"R2 as a function of $\alpha$", fontsize = fs1)
+		ax.set_xlabel(r"$\alpha$", fontsize = fs2)
+		ax.set_ylabel(r"R2", fontsize = fs2)
+		ax.legend()
+
+
+		fig2, ax2 = plt.subplots()
+		ax2.semilogx(alpha_values, MSElist, label = "MSE") 
+		ax2.set_title(r"MSE as a function of $\alpha$", fontsize = fs1)
+		ax2.set_xlabel(r"$\alpha$", fontsize = fs2)
+		ax2.set_ylabel(r"MSE", fontsize = fs2)
+		ax2.legend()
+
+		plt.show()
+
+		#Found the best aplha to be below 1e-3, but those values gives me errors in the scikit lasso.
+		# Did therefore choose a more stable alpha.
+
+	alpha = 1e-2
+	#Without noise
+	pred_lasso_SK = (Lasso(alpha = alpha, fit_intercept = False).fit(X, zr).predict(X)).flatten()
+
+	#With noise
+	pred_lasso_noise_SK = (Lasso(alpha = alpha, fit_intercept = False).fit(X, zr_noise).predict(X)).flatten()
+
+	#Finding MSE and R2
+	R2MSEeval(zr, pred_lasso_SK, method = "Lasso", printer = True, sk = True)
+	R2MSEeval(zr, pred_lasso_noise_SK, method = "Lasso with noise", printer = True, sk = True)
+
+
+	print ("Bootstrap using Lasso without noise:")
+	bootstrap(X, zr, alpha = alpha, method = "Lasso")
+	print ("Bootstrap using Lasso with noise:")
+	bootstrap(X, zr_noise, alpha = alpha, method = "Lasso")
+
+###########################
+#####     Task d)     #####
+###########################
+
+elif argv[1] == "d" or argv == "all":
+
+	terrainReader = MapDataImport()
+	terrainReader.ImportData()
+	terrainReader.PlotTerrain()
+
+	print ("Please, come back later")
+	exit()
+#Import data
+
+
+###########################
+#####     Task e)     #####
+###########################
+
+elif argv[1] == "e" or argv == "all":
+
+	terrainReader = MapDataImport()
+	terrainReader.ImportData()
+
+	n_patches = 10
+
+	#z = terrainReader.terrain.reshape(-1, 1)
+
+	[n, m] = terrainReader.terrain.shape
+
+	patch_n = int(np.floor(n/n_patches))
+	patch_m = int(np.floor(m/n_patches))
+
+	terrain = []
+
+	print (patch_n, patch_m, terrainReader.terrain.shape)
+
+	for i in range(0, n_patches):
+		print (i*patch_n, (i+1)*patch_n)
+
+		terrain.append((terrainReader.terrain)[i*patch_n:(i+1)*patch_n, i*patch_m:(i+1)*patch_m])
+		#Will loose the last row by doing it like this
+
+	rows = np.linspace(0, 1, patch_n)
+	cols = np.linspace(0, 1, patch_m)
+
+	[C, R] = np.meshgrid(cols, rows)
+
+	x = C.reshape(-1, 1)
+	y = R.reshape(-1, 1)
+
+	num_data = n*m
+
+	X = X_creator(x, y, n_samples1 = patch_n, n_samples2 = patch_m)
+
+	
+	for i in terrain:
+		z = i.flatten()
+
+		print(z.shape)
+		print (X.shape)
+
+		bootstrap(X, z, method = "OLS") 
+		bootstrap(X, z, lmb = 1e-4, method = "Ridge")	
+		bootstrap(X, z, alpha = 1e-2, method = "Lasso")
+
+		
+
+		pred = (LinearRegression(fit_intercept = False).fit(X, z).predict(X)).flatten()
+
+		print (pred.shape)
+
+	# plt.figure()
+	# plt.title('Terrain over Norway 1')
+	# plt.imshow(pred.reshape(-1, 1), cmap='gray')
+	# plt.xlabel('X')
+	# plt.ylabel('Y')
+	# plt.show()
+
+
+
+# Perform a), b) and c) on data from d).
+
+
+else:
+	print ("Please write a, b, c, d or e after filename in terminal")
+	exit()
