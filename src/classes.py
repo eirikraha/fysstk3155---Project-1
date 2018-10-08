91487fcfe4b9840597e0be4443c89e311660d1e6
eirikraha <eirikhauge@hotmail.com>
2018-10-08 22:33:38 +0200

    Made the last changes

--- src/classes.py
+++ src/classes.py
@@ -13,6 +13,11 @@ from imageio import imread
 
 
 class HomeMadeOLS():
+	# Ordinaty least squared regression. Fit fit's the data
+	# and predict predicts
+	# ConfIntBeta finds the confidence intervar of the beta values
+	# and plotter plots them
+
 	def __init__(self):
 
 		self.beta = 0
@@ -35,20 +40,38 @@ class HomeMadeOLS():
 
 		variance = 1./(N - p - 1) * np.sum((zr - pred)**2)
 
-		var_beta = [(np.linalg.inv(X.T @ X)*variance)[i, i] for i in range(0, p)]
+		self.var_beta = [(np.linalg.inv(X.T @ X))[i, i] for i in range(0, p)]
 
-		self.conf_intervals = [[float(self.beta[i]) - 2*np.sqrt(var_beta[i]), 
-							float(self.beta[i]) + 2*np.sqrt(var_beta[i])] for i in range(0, len(var_beta))]
+		self.conf_intervals = [[float(self.beta[i]) - 2*np.sqrt(self.var_beta[i]), 
+							float(self.beta[i]) + 2*np.sqrt(self.var_beta[i])] for i in range(0, len(self.var_beta))]
+
+		return self
+
+	def plotter(self, fs1 = 20, fs2 = 20, fs3 = 20, task = 'a', method = 'OLS', patch = 'None'):
+
+		twostd_beta = 2*np.sqrt(self.var_beta)
+
+		plt.errorbar(np.linspace(0, len(self.beta), len(self.beta)), self.beta, yerr=twostd_beta)
+		plt.title(r'$\beta$ confidence for %s' %method, fontsize = fs1)
+		plt.xlabel(r'$\beta_j$', fontsize = fs2)
+		plt.savefig('../figures/%s-%s-contour-beta-patch%s.png' %(task, method, patch))
+		plt.tight_layout()
+
 
 class HomeMadeRidge():
+	# Ridge regression. Fit fit's the data
+	# and predict predicts
+	# ConfIntBeta finds the confidence intervar of the beta values
+	# and plotter plots them
+
 	def __init__(self):
 
 		self.beta = 0
 		self.pred = 0
 
 	def fit(self, X, zr, lmb = 1e-3):
-		Id_mat = np.eye(X.shape[1])
-		self.beta = (np.linalg.inv(X.T @ X + lmb*Id_mat) @ X.T @ zr).flatten()
+		self.Id_mat = np.eye(X.shape[1])
+		self.beta = (np.linalg.inv(X.T @ X + lmb*self.Id_mat) @ X.T @ zr).flatten()
 
 		return self
 
@@ -57,7 +80,32 @@ class HomeMadeRidge():
 
 		return self
 
+	def ConfIntBeta(self, X, zr, pred, lmb = 1e-3):
+		N = X.shape[0]
+		p = X.shape[1]
+
+		variance = 1./(N - p - 1) * np.sum((zr - pred)**2)
+
+		self.var_beta = [(variance*(np.linalg.inv(X.T @ X * lmb * self.Id_mat)) @ X.T @ X @((np.linalg.inv(X.T @ X * lmb * self.Id_mat)).T))[i, i] for i in range(0, p)]
+
+		self.conf_intervals = [[float(self.beta[i]) - 2*np.sqrt(self.var_beta[i]), 
+							float(self.beta[i]) + 2*np.sqrt(self.var_beta[i])] for i in range(0, len(self.var_beta))]
+
+		return self
+
+	def plotter(self, fs1 = 20, fs2 = 20, fs3 = 20, task = 'a', method = 'OLS', patch = 'None'):
+
+		twostd_beta = 2*np.sqrt(self.var_beta)
+
+		plt.errorbar(np.linspace(0, len(self.beta), len(self.beta)), self.beta, yerr=twostd_beta)
+		plt.title(r'$\beta$ confidence for %s' %method, fontsize = fs1)
+		plt.xlabel(r'$\beta_j$', fontsize = fs2)
+		plt.savefig('../figures/%s-%s-contour-beta-patch%s.png' %(task, method, patch))
+		plt.tight_layout()
+
+
 class MapDataImport():
+	#imports and maps data
 	def __init__(self):
 		b = 0
 
